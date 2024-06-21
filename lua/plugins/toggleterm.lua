6a74a7906a572659745d6bf30109a3eb267504b3
Daniel Cefram Ramirez <dc@rmrz.ph>
2024-06-21 22:37:14 +0800

    add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+      {
+        "<leader>td",
+        "<cmd>ToggleTerm<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+    },
+    config = true,
+  },
+}
